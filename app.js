c1a2ec637f2db71b3900a93b316e45da1c6546b5
samanvithms2002 <samanvith2002@gmail.com>
2020-05-18 18:48:37 +0530

    have cahnged the script

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.post("/",function(req,res){
         {
             if(response.statusCode===200)
             res.sendFile(__dirname+"/success.html");
+           
         }
        })
 }) 
@@ -59,6 +60,6 @@ app.post("/failure",function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000,function(req,res){
+app.listen( process.env.PORT||3000,function(req,res){
     console.log("Server is running on port 3000");
 });
